1fd7287663ee690b40e6b5d64297f68323564125
un-earthly <vijayalamin@gmail.com>
2022-05-08 10:00:36 -0700

    pagination for my items

--- src/Pages/MyItems.js
+++ src/Pages/MyItems.js
@@ -7,13 +7,16 @@ import MySkeleton from '../SharedAndUtils/MySkeleton'
 
 export default function MyItems() {
 
+    const [pagesCount, setPagesCount] = useState(0)
+    const [activePage, setActivePage] = useState(0)
+    const [pageSize, setPageSize] = useState(10)
     const [user] = useAuthState(auth)
     const [myItems, setMyItems] = useState([])
     const [loading, setLoading] = useState(true)
+    const email = user.email
     useEffect(() => {
         const getOrders = async () => {
-            const email = user.email;
-            const url = `https://guarded-shelf-11836.herokuapp.com/byemail?email=${email}`;
+            const url = `https://guarded-shelf-11836.herokuapp.com/byemail?email=${email}&pageSize=${pageSize}&activePage=${activePage}`;
             const { data } = await axios.get(url, {
                 headers: {
                     authorization: `Bearer ${localStorage.getItem('token')}`
@@ -23,7 +26,12 @@ export default function MyItems() {
             setMyItems(data);
         }
         getOrders();
-    }, [user, myItems])
+    }, [user, myItems, pageSize, activePage, email])
+
+    useEffect(() => {
+        axios.get(`https://guarded-shelf-11836.herokuapp.com/pageCountEmail?email=${email}`)
+            .then(data => setPagesCount(Math.ceil(data.data.count / pageSize)))
+    }, [pageSize, email])
     return (
         <div>
             <div className="grid grid-cols-1 lg:grid-cols-2 gap-3 xl:gap-10 xl:p-7">
@@ -43,6 +51,20 @@ export default function MyItems() {
                         :
                         myItems.map(item => <InventoryItems key={item._id} itemData={item} />)
                 }
+            </div>
+
+            <div className='mx-auto flex items-center justify-center mt-6 space-x-2'>{
+                [...Array(pagesCount).keys()].map(page => <button key={page} className={`px-2 border block border-red-500 ${activePage === page ? 'pageActive' : ''}`} onClick={() => { setActivePage(page) }}>{(page + 1)}</button>)
+
+            }
+                <select className='outline-none bg-red-500 py-1 px-1 text-white' onClick={e => setPageSize(e.target.value)}>
+                    <option value='2'>2</option>
+                    <option value='5'>5</option>
+                    <option selected value='10'>10</option>
+                    <option value='15'>15</option>
+                    <option value='20'>20</option>
+                </select>
+                <div> <p>{pageSize} items per page</p></div>
             </div>
         </div>
     )
