d1d34f71a17347f9f9472a5b1c6a8a44252d914f
un-earthly <vijayalamin@gmail.com>
2022-05-08 03:16:46 -0700

    contact form

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,34 +1,37 @@
 import React from 'react'
+import { toast } from 'react-toastify'
 import Button from '../SharedAndUtils/Button'
 
 export default function Contact() {
     return (
-        <form className="md:w-1/2 mt-4 rounded-md mx-auto shadow-md p-5">
+        <form onSubmit={e => {
+            e.preventDefault()
+            toast('Successfull')
+            e.target.reset()
+        }} className="md:w-1/2 mt-4 rounded-md mx-auto shadow-md p-5">
             <div className="w-full mb-6 md:mb-0">
                 <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
-                    First Name
+                    Name
                 </label>
-                <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-800  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Name" />
+                <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-800  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Name" required />
             </div>
             <div className="flex flex-wrap -mx-3 mb-6">
                 <div className="w-full px-3">
-                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="email">
                         E-mail
                     </label>
-                    <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-800 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="email" type="email" />
+                    <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-800 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="email" type="email" required placeholder="Email" />
                 </div>
             </div>
             <div className="flex flex-wrap -mx-3 mb-6">
                 <div className="w-full px-3">
-                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="message">
                         Message
                     </label>
-                    <textarea className=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-800 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none" id="message"></textarea>
+                    <textarea className=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-800 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none" id="message" required></textarea>
                 </div>
             </div>
-            <div className="md:flex md:items-center">
-                <Button btnText='Send' classes='block w-full' />
-            </div>
+            <Button btnText='Send' classes='block w-full' />
         </form>
     )
 }
