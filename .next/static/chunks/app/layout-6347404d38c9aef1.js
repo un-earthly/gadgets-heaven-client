d858a843363588d387111a503d801bde09623a52
un-earthly <vijayalamin@gmail.com>
2025-02-03 23:51:07 +0600

    fixed version conflits

---
+++ .next/static/chunks/app/layout-6347404d38c9aef1.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[7177],{6397:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,9324,23))},9324:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[2533,8441,1517,7358],()=>s(6397)),_N_E=e.O()}]);
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState } from 'react';
+import React from 'react';
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import {
