31e5a604220f1ee6583441a406d4b93229e66df8
un-earthly <vijayalamin@gmail.com>
2025-02-06 04:24:49 +0600

    project migrated

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export', // Outputs a Single-Page Application (SPA).
-    distDir: './build', // Changes the build output directory to `./build`.
-}
+import type { NextConfig } from "next";
 
-export default nextConfig
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+export default nextConfig;
